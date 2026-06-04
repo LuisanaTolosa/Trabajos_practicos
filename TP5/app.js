cd037fe9ed8216e645ff551ca83359994cdb6dc9
alumno <51048512@ivt.edu.ar>
2026-06-04 09:07:34 -0300

    04/06/26

--- TP5/app.js
+++ TP5/app.js
@@ -1,8 +1,23 @@
-let numero1 = 25;
-let numero2 = 23;
-let resultado
+let suma= 0;
+let resta= 0;
+let divicion= 0;
+let multiplicacion=0;
 
-resultado = numero1 + numero2;
+let numero1=5;
+let numero2=10;
 
-console.log(resultado);
-alert(resultado)
+suma= numero1 + numero2
+resta= numero2 - numero1
+multiplicacion= numero1 * numero2
+division= numero1/numero2
+
+
+console.log("el resultado de la multiplicacion es:"+multiplicacion);
+console.log("el resultado de la division es:"+division);
+alert ( "el resultado de la suma:"+suma);
+alert ("el resultado de la resta:"+resta)
+
+let nombrealumna = 'luisana'
+let apellidoalumna = 'tolosa'
+let edad = 14
+console.log ("la alumna es" + nombrealumna + apellidoalumna)
